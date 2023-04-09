1d163ec7a9b1e960b4eb8d5a035595788bbd04d2
Евгений Андреевич Нечаев <nechaev_ea@office.osu.ru>
2023-04-09 20:17:00 +0500

    Add dbContext and repositories serviceExtensions

--- src/CheckByStopBase.CompanyStopBase.DAL/ServiceCollectionExtensions.cs
+++ src/CheckByStopBase.CompanyStopBase.DAL/ServiceCollectionExtensions.cs
@@ -1,5 +1,28 @@
-﻿namespace CheckByStopBase.CompanyStopBase.DAL;
+﻿using CheckByStopBase.CompanyStopBase.DAL.DataContext;
+using CheckByStopBase.CompanyStopBase.DAL.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CheckByStopBase.CompanyStopBase.DAL;
 
 public static class ServiceCollectionExtensions
 {
+    public static void AddCompanyRepositories(this IServiceCollection collection, string connectionString)
+    {
+        collection.AddCompanyPostgresSqlRepository<ICompanyRegistryRepository, CompanyRegistryRepository, CompanyDbContext>(connectionString);
+        collection.AddCompanyPostgresSqlRepository<ICompanyReportRepository, CompanyReportRepository, CompanyDbContext>(connectionString);
+    }
+
+    private static void AddCompanyPostgresSqlRepository<TRepository, TRepositoryImplementation, TRepositoryDbContext>(
+        this IServiceCollection collection, string connectionString)
+        where TRepository : class
+        where TRepositoryImplementation : class, TRepository
+        where TRepositoryDbContext : DbContext
+    {
+        collection.AddDbContextPool<TRepositoryDbContext>(builder =>
+            builder.UseNpgsql(connectionString)
+        );
+
+        collection.AddScoped<TRepository, TRepositoryImplementation>();
+    }
 }
